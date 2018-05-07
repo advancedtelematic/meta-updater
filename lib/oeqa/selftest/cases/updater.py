98b1cb7a2c90a203d885663ed2b6cc1bae08f5b3
Patrick Vacek <patrickvacek@gmail.com>
2018-05-07 11:24:45 +0200

    Give some more time for checking for successful provisioning.

--- lib/oeqa/selftest/cases/updater.py
+++ lib/oeqa/selftest/cases/updater.py
@@ -671,12 +671,12 @@ def akt_native_run(testInst, cmd, **kwargs):
 def verifyProvisioned(testInst, machine):
     # Verify that device HAS provisioned.
     ran_ok = False
-    for delay in [5, 5, 5, 5, 10]:
-        sleep(delay)
+    for delay in [5, 5, 5, 5, 10, 10, 10, 10]:
         stdout, stderr, retcode = testInst.qemu_command('aktualizr-info')
         if retcode == 0 and stderr == b'' and stdout.decode().find('Fetched metadata: yes') >= 0:
             ran_ok = True
             break
+        sleep(delay)
     testInst.assertIn(b'Device ID: ', stdout, 'Provisioning failed: ' + stderr.decode() + stdout.decode())
     testInst.assertIn(b'Primary ecu hardware ID: ' + machine.encode(), stdout,
                   'Provisioning failed: ' + stderr.decode() + stdout.decode())
