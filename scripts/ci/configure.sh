a3c960df21c5f820cd7aa592a449be667f65cf86
Laurent Bonnans <laurent.bonnans@here.com>
2018-06-22 15:09:57 +0200

    Add caching to CI bitbaking
    Also fix various issues with environment variables and file transfers to
    Docker

--- scripts/ci/configure.sh
+++ scripts/ci/configure.sh
@@ -9,7 +9,8 @@ TEST_REPO_DIR=${TEST_REPO_DIR:-updater-repo}
 
 TEST_AKTUALIZR_DIR=${TEST_AKTUALIZR_DIR:-.}
 TEST_AKTUALIZR_BRANCH=${TEST_AKTUALIZR_BRANCH:-master}
-TEST_AKTUALIZR_REV=${TEST_AKTUALIZR_REV:-$(GIT_DIR="${TEST_AKTUALIZR_DIR}/.git" git rev-parse "${TEST_AKTUALIZR_BRANCH}")}
+TEST_AKTUALIZR_REV=${TEST_AKTUALIZR_REV:-$(GIT_DIR="$TEST_AKTUALIZR_DIR/.git" git rev-parse "$TEST_AKTUALIZR_REMOTE/$TEST_AKTUALIZR_BRANCH")}
+TEST_BITBAKE_COMMON_DIR=${TEST_BITBAKE_COMMON_DIR:-}
 
 # move existing conf directory to backup, before generating a new one
 rm -rf "${TEST_BUILD_DIR}/conf.old" || true
@@ -22,12 +23,32 @@ echo ">> Running envsetup.sh"
 . "${TEST_REPO_DIR}/meta-updater/scripts/envsetup.sh" "${TEST_MACHINE}" "${TEST_BUILD_DIR}"
 )
 
-echo ">> Set aktualizr branch in bitbake's config"
+set +x
 
+echo ">> Set common bitbake config options"
 cat << EOF > "${TEST_BUILD_DIR}/conf/site.conf"
 SANITY_TESTED_DISTROS = ""
+SSTATE_MIRRORS ?= "file://.* https://bitbake-cache.atsgarage.com/PATH;downloadfilename=PATH"
+IMAGE_FEATURES += "ssh-server-openssh"
+
+EOF
+
+echo ">> Set aktualizr branch in bitbake's config"
+cat << EOF >> "${TEST_BUILD_DIR}/conf/site.conf"
 SRCREV_pn-aktualizr = "$TEST_AKTUALIZR_REV"
 SRCREV_pn-aktualizr-native = "\${SRCREV_pn-aktualizr}"
 BRANCH_pn-aktualizr = "$TEST_AKTUALIZR_BRANCH"
 BRANCH_pn-aktualizr-native = "\${BRANCH_pn-aktualizr}"
+
 EOF
+
+if [[ -n $TEST_BITBAKE_COMMON_DIR ]]; then
+    echo ">> Set caching"
+    SSTATE_DIR="$TEST_BITBAKE_COMMON_DIR/sstate-cache"
+    DL_DIR="$TEST_BITBAKE_COMMON_DIR/downloads"
+    mkdir -p "$SSTATE_DIR" "$DL_DIR"
+    cat << EOF >> "${TEST_BUILD_DIR}/conf/site.conf"
+SSTATE_DIR = "$SSTATE_DIR"
+DL_DIR = "$DL_DIR"
+EOF
+fi
