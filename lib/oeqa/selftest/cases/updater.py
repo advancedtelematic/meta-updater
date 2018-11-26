de6008d854ddc94589a050e7eac00ccd1cf072a6
Patrick Vacek <patrickvacek@gmail.com>
2018-11-26 15:05:59 +0100

    oe-selftest: rpi-basic-image is deprecated. Use core-image-minimal.
    Signed-off-by: Patrick Vacek <patrickvacek@gmail.com>

--- lib/oeqa/selftest/cases/updater.py
+++ lib/oeqa/selftest/cases/updater.py
@@ -285,9 +285,9 @@ class RpiTests(OESelftestTestCase):
 
     def test_rpi(self):
         logger = logging.getLogger("selftest")
-        logger.info('Running bitbake to build rpi-basic-image')
+        logger.info('Running bitbake to build core-image-minimal')
         self.append_config('SOTA_CLIENT_PROV = "aktualizr-auto-prov"')
-        bitbake('rpi-basic-image')
+        bitbake('core-image-minimal')
         credentials = get_bb_var('SOTA_PACKED_CREDENTIALS')
         # Skip the test if the variable SOTA_PACKED_CREDENTIALS is not set.
         if credentials is None:
@@ -295,7 +295,7 @@ class RpiTests(OESelftestTestCase):
         # Check if the file exists.
         self.assertTrue(os.path.isfile(credentials), "File %s does not exist" % credentials)
         deploydir = get_bb_var('DEPLOY_DIR_IMAGE')
-        imagename = get_bb_var('IMAGE_LINK_NAME', 'rpi-basic-image')
+        imagename = get_bb_var('IMAGE_LINK_NAME', 'core-image-minimal')
         # Check if the credentials are included in the output image.
         result = runCmd('tar -jtvf %s/%s.tar.bz2 | grep sota_provisioning_credentials.zip' %
                         (deploydir, imagename), ignore_status=True)
