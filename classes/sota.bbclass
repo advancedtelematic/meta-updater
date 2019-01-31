advancedtelematic/meta-updater

commit 9dcfcdb9aa2da833a3b86b87fbbb22b60e5d7e3c

classes, images: Use standard variables for initramfs
This will reduce the level of indirection and improve compatibility
with the rest of OE.
For example, FIT that includes initramfs may now be built.

Signed-off-by: Yevgeny Popovych <yevgenyp@pointgrab.com>

classes/sota.bbclass
@@ -23,13 +23,13 @@ WKS_FILE_sota ?= "sdimage-sota.wks"
 
 EXTRA_IMAGEDEPENDS_append_sota = " parted-native mtools-native dosfstools-native"
 
-OSTREE_INITRAMFS_FSTYPES ??= "${@oe.utils.ifelse(d.getVar('OSTREE_BOOTLOADER', True) == 'u-boot', 'ext4.gz.u-boot', 'ext4.gz')}"
+INITRAMFS_FSTYPES ??= "${@oe.utils.ifelse(d.getVar('OSTREE_BOOTLOADER', True) == 'u-boot', 'ext4.gz.u-boot', 'ext4.gz')}"
 
 # Please redefine OSTREE_REPO in order to have a persistent OSTree repo
 OSTREE_REPO ?= "${DEPLOY_DIR_IMAGE}/ostree_repo"
 OSTREE_BRANCHNAME ?= "${MACHINE}"
 OSTREE_OSNAME ?= "poky"
-OSTREE_INITRAMFS_IMAGE ?= "initramfs-ostree-image"
+INITRAMFS_IMAGE ?= "initramfs-ostree-image"
 OSTREE_BOOTLOADER ??= 'u-boot'
 
 GARAGE_SIGN_REPO ?= "${DEPLOY_DIR_IMAGE}/garage_sign_repo"

classes/image_types_ostree.bbclass
@@ -5,13 +5,11 @@ do_image_ostree[depends] += "ostree-native:do_populate_sysroot \
                         coreutils-native:do_populate_sysroot \
                         unzip-native:do_populate_sysroot \
                         virtual/kernel:do_deploy \
-                        ${OSTREE_INITRAMFS_IMAGE}:do_image_complete"
+"
 
 export OSTREE_REPO
 export OSTREE_BRANCHNAME
 export GARAGE_TARGET_NAME
-
-RAMDISK_EXT ?= ".${OSTREE_INITRAMFS_FSTYPES}"
 
 OSTREE_KERNEL ??= "${KERNEL_IMAGETYPE}"
 
@@ -124,7 +122,7 @@ IMAGE_CMD_ostree () {
     checksum=`sha256sum ${DEPLOY_DIR_IMAGE}/${OSTREE_KERNEL} | cut -f 1 -d " "`
 
     cp ${DEPLOY_DIR_IMAGE}/${OSTREE_KERNEL} boot/vmlinuz-${checksum}
-    cp ${DEPLOY_DIR_IMAGE}/${OSTREE_INITRAMFS_IMAGE}-${MACHINE}${RAMDISK_EXT} boot/initramfs-${checksum}
+    cp ${DEPLOY_DIR_IMAGE}/${INITRAMFS_IMAGE_NAME}.${INITRAMFS_FSTYPES} boot/initramfs-${checksum}
 
     # Copy image manifest
     cat ${IMAGE_MANIFEST} | cut -d " " -f1,3 > usr/package.manifest

classes/sota_qemux86-64.bbclass
@@ -4,7 +4,7 @@ IMAGE_FSTYPES_remove = "wic"
 PREFERRED_PROVIDER_virtual/bootloader_sota = "u-boot"
 UBOOT_MACHINE_sota = "qemu-x86_defconfig"
 OSTREE_BOOTLOADER ?= "u-boot"
-OSTREE_INITRAMFS_FSTYPES ?= "ext4.gz"
+INITRAMFS_FSTYPES ?= "ext4.gz"
 
 OSTREE_KERNEL_ARGS ?= "ramdisk_size=16384 rw rootfstype=ext4 rootwait rootdelay=2 ostree_root=/dev/hda"
 

classes/sota_raspberrypi.bbclass
@@ -2,6 +2,7 @@ RPI_USE_U_BOOT_sota = "1"
 
 KERNEL_CLASSES_append_sota = " kernel-fitimage"
 KERNEL_IMAGETYPE_sota = "fitImage"
+OSTREE_KERNEL = "${KERNEL_IMAGETYPE}-${INITRAMFS_IMAGE}-${MACHINE}.bin"
 
 PREFERRED_PROVIDER_virtual/bootloader_sota ?= "u-boot"
 UBOOT_ENTRYPOINT_sota ?= "0x00008000"

recipes-core/images/initramfs-ostree-image.bb
@@ -13,7 +13,7 @@ IMAGE_LINGUAS = ""
 
 LICENSE = "MIT"
 
-IMAGE_FSTYPES = "${OSTREE_INITRAMFS_FSTYPES}"
+IMAGE_FSTYPES = "${INITRAMFS_FSTYPES}"
 
 inherit core-image
 

commit 168efb38411ed4feae6d99e4ae00c867ceff3fba

sota.bbclass: fix a variable assignment regression
A regression was introduced by commit 9dcfcdb9:
[ classes, images: Use standard variables for initramfs ]

it replaced OSTREE_INITRAMFS_IMAGE with INITRAMFS_FSTYPES which is
fine, but the '??=' should be changed to "?=" as well, or else it will
not take effect since INITRAMFS_FSTYPES's already set in bitbake.conf
with '?='.

Signed-off-by: Ming Liu <liu.ming50@gmail.com>

classes/sota.bbclass
@@ -21,7 +21,7 @@ WKS_FILE_sota ?= "sdimage-sota.wks"
 
 EXTRA_IMAGEDEPENDS_append_sota = " parted-native mtools-native dosfstools-native"
 
-INITRAMFS_FSTYPES ??= "${@oe.utils.ifelse(d.getVar('OSTREE_BOOTLOADER') == 'u-boot', 'cpio.gz.u-boot', 'cpio.gz')}"
+INITRAMFS_FSTYPES ?= "${@oe.utils.ifelse(d.getVar('OSTREE_BOOTLOADER') == 'u-boot', 'cpio.gz.u-boot', 'cpio.gz')}"
 
 # Please redefine OSTREE_REPO in order to have a persistent OSTree repo
 export OSTREE_REPO ?= "${DEPLOY_DIR_IMAGE}/ostree_repo"