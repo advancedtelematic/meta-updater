7679da3a7ada003c112e68d4c6e317466f0ccc7e
Zee314159 <252806294@qq.com>
2019-07-24 09:32:24 +0200

    oe-selftests: wait until provisioned
    * fixed bugs
    * Removed unnecessary code and added comments
    * Checked stderr and stdout
    
    Signed-off-by: Zee314159 <252806294@qq.com>
    (cherry-picked and squashed from PR #562)

--- lib/oeqa/selftest/cases/testutils.py
+++ lib/oeqa/selftest/cases/testutils.py
@@ -121,15 +121,21 @@ def verifyNotProvisioned(testInst, machine):
 
 def verifyProvisioned(testInst, machine):
     # Verify that device HAS provisioned.
+    # First loop while waiting for the device to boot.
     ran_ok = False
     for delay in [5, 5, 5, 5, 10, 10, 10, 10]:
         stdout, stderr, retcode = testInst.qemu_command('aktualizr-info')
-        if retcode == 0 and stderr == b'' and stdout.decode().find('Fetched metadata: yes') >= 0:
+        if retcode == 0 and stderr == b'':
             ran_ok = True
             break
         sleep(delay)
     testInst.assertTrue(ran_ok, 'aktualizr-info failed: ' + stderr.decode() + stdout.decode())
+    # Then wait for aktualizr to provision.
+    if stdout.decode().find('Fetched metadata: yes') < 0:
+        stdout, stderr, retcode = testInst.qemu_command('aktualizr-info --wait-until-provisioned')
 
+    testInst.assertFalse(retcode, 'aktualizr-info failed: ' + stderr.decode() + stdout.decode())
+    testInst.assertEqual(stderr, b'', 'aktualizr-info failed: ' + stderr.decode() + stdout.decode())
     testInst.assertIn(b'Device ID: ', stdout, 'Provisioning failed: ' + stderr.decode() + stdout.decode())
     testInst.assertIn(b'Primary ecu hardware ID: ' + machine.encode(), stdout,
                       'Provisioning failed: ' + stderr.decode() + stdout.decode())
