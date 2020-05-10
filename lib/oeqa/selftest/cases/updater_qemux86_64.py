0792fdc820e5a3c159f88156bec9ff3eb013a6d6
Ming Liu <ming.liu@toradex.com>
2020-05-10 10:36:20 +0200

    meta: change virtual/network-configuration to network-configuration
    The name 'virtual' is a recipe specific concept but not a package
    specific one, it's confusing that a package provides 'virtual/' names.
    
    Let's drop 'virtual/' from network-configuration, to keep consistent
    with yocto naming styles.
    
    Signed-off-by: Ming Liu <ming.liu@toradex.com>

--- lib/oeqa/selftest/cases/updater_qemux86_64.py
+++ lib/oeqa/selftest/cases/updater_qemux86_64.py
@@ -130,7 +130,7 @@ class SharedCredProvTestsNonOSTree(SharedCredProvTests):
         self.append_config('DISTRO = "poky"')
         self.append_config('DISTRO_FEATURES_append = " systemd"')
         self.append_config('VIRTUAL-RUNTIME_init_manager = "systemd"')
-        self.append_config('PREFERRED_RPROVIDER_virtual/network-configuration ??= "networkd-dhcp-conf"')
+        self.append_config('PREFERRED_RPROVIDER_network-configuration ??= "networkd-dhcp-conf"')
         self.append_config('PACKAGECONFIG_pn-aktualizr = ""')
         self.append_config('SOTA_DEPLOY_CREDENTIALS = "1"')
         self.append_config('IMAGE_INSTALL_append += "aktualizr"')
